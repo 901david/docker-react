875326c1c1ef336164bbfe5baa68d94197af2634
901david <vdavidhammond@gmail.com>
2019-07-21 11:21:56 -0600

    Set up API/Client app with docker and docker compose

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("api/data", (req, res) => {
+  res.json({ data: [{ id: 1, name: "some Person" }] });
+});
+
+app.listen(PORT, err => {
+  if (err) throw err;
+
+  console.log(`Ready for incoming requests on port: ${PORT}`);
+});
