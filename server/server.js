1b1109462430a277f8c939c431d0e93005e96c6e
901david <vdavidhammond@gmail.com>
2019-08-04 06:37:46 -0600

    added in production docker files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get("api/data", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ data: [{ id: 1, name: "some Person" }] });
 });
 
